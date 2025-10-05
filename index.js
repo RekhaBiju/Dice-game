7474bb59c98a68c2d038ab8a018438992ec00891
Rekha B <rekhabiju1478@gmail.com>
2025-10-05 18:43:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function rollDice() {
+let random1 = Math.floor(Math.random() * 6) + 1;
+let random2 = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector(".img1").setAttribute("src", "images/dice" + random1 + ".png");
+document.querySelector(".img2").setAttribute("src", "images/dice" + random2 + ".png");
+
+if (random1 > random2) {
+  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+} else if (random2 > random1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+} else {
+  document.querySelector("h1").innerHTML = "It's a Draw! 🎲";
+}
+}
